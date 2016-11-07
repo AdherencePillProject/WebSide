c97e71f774b5d726078ffa730864448a02d46994
Dicky Shi <DickyShi@DickyShi.local>
2016-11-07 12:56:31 -0600

    reduce callback hell

--- AdherencePillProject/routes/user.js
+++ AdherencePillProject/routes/user.js
@@ -1,44 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var utility = require('./utility');
+var checkSession = utility.checkSession;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var sessionToken = req.get("x-parse-session-token");
-  Parse.User.become(sessionToken, {
+  checkSession(req.get("x-parse-session-token"), {
     success: function(user) {
-      if (user) {
-        var type = 0;
-        if (user.get("patientPointer") !== undefined) {
-          type += 1;
-        }
-        if (user.get("doctorPointer") !== undefined) {
-          type += 10;
-        }
-        console.log(type);
-        res.status(200).json({
-          code: 1,
-          email: user.get("email"),
-          firstname: user.get("firstname"),
-          lastname: user.get("lastname"),
-          phone: user.get("phone"),
-          gender: user.get("gender"),
-          dateOfBirth: user.get("dateOfBirth"),
-          type: type,
-          patientPointer: user.get("patientPointer"),
-          doctorPointer: user.get("doctorPointer")
-        });
-      }
-      else {
-        res.status(401)
-          .json({code: 209, messgae: "invalid session token"});
-      }
+      res.status(200).json({
+        code: 1,
+        email: user.get("email"),
+        firstname: user.get("firstname"),
+        lastname: user.get("lastname"),
+        phone: user.get("phone"),
+        gender: user.get("gender"),
+        dateOfBirth: user.get("dateOfBirth"),
+        type: user.get("type"),
+        patientPointer: user.get("patientPointer"),
+        doctorPointer: user.get("doctorPointer")
+      });
     },
     error: function(error) {
       res.status(401)
         .json({"code": error.code, "message": error.message});
     }
-  })
-
+  });//checkSession
 });
 router.post('/', function(req, res, next) {
   console.log(req);

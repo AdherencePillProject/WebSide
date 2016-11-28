f9ba24580e99c05b394fcfbdc7fad028210a1645
SHvsMK <SHvsMK@gmail.com>
2016-11-28 15:56:52 -0600

    Refactory the structure of Router
    * Move /login /logout to the index.js
    * Create controller file to save the function of each router.
    * Create /account router in the index.
    * fix a bug, change email server URL

--- AdherencePillProject/routes/index.js
+++ AdherencePillProject/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../controller');
+var login = controller.login;
+var logout = controller.logout;
+var account = controller.account;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* login */
+router.post('/login', login.login);
+
+/* logout */
+router.get('/logout', logout.logout);
+
+///* activate email, the name of this API should change to /account/verification, this will be changed to the PUT method */
+//router.get('/account/email', account.activateEmail);
+//
+///* send the email to the user's email for resetting password, this function's name should be better */
+//router.get('/account/password', account.sendPasswordResettingEmail);
+//
+///* update password */
+//router.put('/account/password', account.resetPassword);
+//
+///* update basic information of a user, do we need to combine updata password with basic information, is there any
+// * place for change password in the user's profile page?
+// */
+//router.put('/account/info', account.editAccount);
 
 module.exports = router;

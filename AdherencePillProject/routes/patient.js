879d73a03d6413c6149e24ba3c5594ebdd9a35ab
SHvsMK <SHvsMK@gmail.com>
2017-02-23 16:15:17 -0600

    API for retrieve patient prescriptions by giving sessionToken

--- AdherencePillProject/routes/patient.js
+++ AdherencePillProject/routes/patient.js
@@ -267,4 +267,55 @@ router.get('/bottles', function(req, res) {
   });
 });
 
+router.get('/prescriptions', function(req, res) {
+  var sessionToken = req.get("x-parse-session-token");
+  checkSession(sessionToken, {
+    success: function success(user) {
+      isPatient(user.id, {
+        success: function success(patient) {
+          var Prescription = new Parse.Object.extend("Prescription");
+          var query = new Parse.Query(Prescription);
+          query.select("name", "note", "pill", "newAdded", "bottle.name", "bottle.pillNumber", "schedule");
+          query.equalTo("patient", patient);
+          query.include("schedule");
+          query.include("bottle");
+          query.find({
+            success: function success(prescriptions) {
+              var ret = [];
+              for (var i = 0; i < prescriptions.length; ++i) {
+                var bottle = {};
+                if (prescriptions[i].get("bottle") !== undefined) {
+                  bottle = {
+                    bottleName: prescriptions[i].get("bottle").get("name"),
+                    pillNumber: prescriptions[i].get("bottle").get("pillNumber"),
+                  };
+                }
+                var entry = {
+                  prescriptionName: prescriptions[i].get("name"),
+                  bottle: bottle,
+                  note: prescriptions[i].get("note"),
+                  schedule: prescriptions[i].get("schedule"),
+                  pill: prescriptions[i].get("pill"),
+                  newAdded: prescriptions[i].get("newAdded")
+                };
+                ret.push(entry);
+              }
+              res.json(ret);
+            },
+            error: function error(err) {
+              res.status(400).json(err);
+            }
+          });
+        },
+        error: function error(err) {
+          res.status(400).json(err);
+        }
+      })
+    },
+    error: function error(err) {
+      res.status(400).json(err);
+    }
+  })
+});
+
 module.exports = router;

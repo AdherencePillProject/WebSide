94ff455edb02f84782023035765466b658b1744b
Dauphin-NWU <seu.wuqihong@gmail.com>
2017-05-27 13:34:54 -0500

    I try to change some attributes to user JSONObject. But the result does not follow my expectation. There might be two explanations: 1. I used the wrong code 2. The project is not the one being deployed on the server

--- AdherencePillProject/controller/login.js
+++ AdherencePillProject/controller/login.js
@@ -46,7 +46,7 @@ exports.login = function(req, res) {
     } else {
         Parse.User.logIn(username, password, {
             success: function (user) {
-                res.json({"code": 1, "sessionToken": user.attributes.sessionToken, "bottle": "SC36-05  4C:55:CC:10:7B:12"});
+                res.json({"code": 1, "sessionToken": user.attributes.sessionToken, "bottle": 2});
             },
             error: function (user, error) {
                 console.log(error);
